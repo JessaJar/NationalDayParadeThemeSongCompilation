5360ca29cd369b65780f837f1e85c5789218ddd2
James <20014241@myrp.edu.sg>
2021-07-16 00:08:05 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20014241/nationaldayparadethemesongcompilation/Song.java
@@ -0,0 +1,68 @@
+package sg.edu.rp.c346.id20014241.nationaldayparadethemesongcompilation;
+
+import java.io.Serializable;
+
+public class Song implements Serializable {
+
+    private int id;
+    private String title;
+    private String singers;
+    private int yearReleased;
+    private int stars;
+
+    public Song(String title, String singers, int yearReleased, int stars) {
+        this.title = title;
+        this.singers = singers;
+        this.yearReleased = yearReleased;
+        this.stars = stars;
+    }
+
+    public Song(int id, String title, String singers, int yearReleased, int stars) {
+        this.id = id;
+        this.title = title;
+        this.singers = singers;
+        this.yearReleased = yearReleased;
+        this.stars = stars;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Song setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setSingers(String singers) {
+        this.singers = singers;
+    }
+
+    public void setYearReleased(int yearReleased) {
+        this.yearReleased = yearReleased;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYear() {
+        return yearReleased;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+}
